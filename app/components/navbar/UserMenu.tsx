6569873a1494d5d39b50332ba9b579a4b37f182e
Ndyabagye Henry <ndyabagyehenrytusi@gmail.com>
2023-04-20 17:41:41 +0300

    Login and registration done, categories, started on creation of homes

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -5,20 +5,39 @@ import Avatar from "@/app/components/Avatar";
 import {useCallback, useState} from "react";
 import MenuItem from "@/app/components/navbar/MenuItem";
 import useRegisterModal from "@/app/hooks/useRegisterModal";
+import useLoginModal from "@/app/hooks/useLoginModal";
+import useRentModal from "@/app/hooks/useRentModal";
 
-const UserMenu = () => {
+import {signOut} from "next-auth/react";
+import {SafeUser} from "@/app/types";
+
+interface UserMenuProps {
+    currentUser?: SafeUser | null;
+}
+
+const UserMenu: React.FC<UserMenuProps> = ({currentUser}) => {
     const registerModal = useRegisterModal();
+    const loginModal = useLoginModal();
+    const rentModal = useRentModal();
     const [isOpen, setIsOpen] = useState(false);
 
     const toggleOpen = useCallback(() => {
         setIsOpen((value) => !value);
     }, []);
 
+    const onRent = useCallback(()=>{
+        if(!currentUser){
+           return loginModal.onOpen();
+        }
+
+        // open rent modal
+        rentModal.onOpen()
+    }, [currentUser, loginModal, rentModal]);
+
     return (
         <div className="relative">
             <div className="flex flex-row items-center gap-3">
-                <div onClick={() => {
-                }} className="hidden md:block text-sm font-smibold py-3 px-4 rounded-full hover:bg-neutral-500
+                <div onClick={onRent} className="hidden md:block text-sm font-smibold py-3 px-4 rounded-full hover:bg-neutral-500
                 transition cursor-pointer">
                     Airbnb your home
                 </div>
@@ -26,7 +45,7 @@ const UserMenu = () => {
                 items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition">
                     <AiOutlineMenu/>
                     <div className="hidden md:block">
-                        <Avatar/>
+                        <Avatar src={currentUser?.image}/>
                     </div>
                 </div>
             </div>
@@ -34,12 +53,24 @@ const UserMenu = () => {
                 <div className="absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12
                 text-sm">
                     <div className="flex flex-col cursor-pointer">
-                        <>
-                            <MenuItem onClick={() => {
-                            }} label="Login"/>
-                            <MenuItem onClick={registerModal.onOpen} label="Sign Up"/>
+                        {currentUser ? (
+                            <>
+                                <MenuItem onClick={()=>{}} label="My Trips"/>
+                                <MenuItem onClick={()=>{}} label="My Favorites"/>
+                                <MenuItem onClick={()=>{}} label="My Reservations"/>
+                                <MenuItem onClick={()=>{}} label="My Properties"/>
+                                <MenuItem onClick={rentModal.onOpen} label="Airbnb my home"/>
+                                <hr/>
+                                <MenuItem onClick={()=> signOut()} label="Logout"/>
 
-                        </>
+                            </>
+                            ): (
+                            <>
+                                <MenuItem onClick={loginModal.onOpen} label="Login"/>
+                                <MenuItem onClick={registerModal.onOpen} label="Sign Up"/>
+
+                            </>
+                            )}
                     </div>
                 </div>
             )}
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,10 @@ import './globals.css'
 import Navbar from "@/app/components/navbar/Navbar";
 import ClientOnly from "@/app/components/ClientOnly";
 import RegisterModal from '@/app/components/Modals/RegisterModal';
+import LoginModal from "@/app/components/Modals/LoginModal";
+import RentModal from "@/app/components/Modals/RentModal";
 import ToasterProvider from "@/app/providers/ToastProvider";
+import getCurrentUser from "@/app/actions/getCurrentUser";
 
 export const metadata = {
     title: 'AirBnb',
@@ -14,19 +17,22 @@ const font = Nunito({
     subsets: ["latin"],
 })
 
-export default function RootLayout({
+export default async function RootLayout({
     children,
 }: {
     children: React.ReactNode
 }) {
-    // @ts-ignore
+    const currentUser = await getCurrentUser();
+
     return (
         <html lang="en">
             <body className={font.className}>
                 <ClientOnly>
                     <ToasterProvider/>
                     <RegisterModal/>
-                    <Navbar />
+                    <LoginModal/>
+                    <RentModal/>
+                    <Navbar currentUser={currentUser}/>
                 </ClientOnly>
 
                 {children}</body>
