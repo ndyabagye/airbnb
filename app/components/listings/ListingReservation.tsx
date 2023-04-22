5db4b4cbfee775feda93759c449680e27a94dae9
Ndyabagye Henry <ndyabagyehenrytusi@gmail.com>
2023-04-22 17:23:29 +0300

    Add reservations

--- app/components/listings/ListingReservation.tsx
+++ app/components/listings/ListingReservation.tsx
@@ -2,6 +2,10 @@
 
 import React from 'react';
 
+import {Range} from 'react-date-range';
+import Calender from "@/app/components/Inputs/Calender";
+import Button from "@/app/components/Button";
+
 interface ListingReservationProps{
     price: number;
     dateRange: Range;
@@ -12,10 +16,31 @@ interface ListingReservationProps{
     disabledDates: Date[]
 
 }
-const ListingReservation: React.FC<ListingReservationProps> = ({}) => {
+const ListingReservation: React.FC<ListingReservationProps> = ({price, dateRange, totalPrice,onChangeDate, onSubmit, disabled, disabledDates}) => {
     return (
-        <div>
-
+        <div className="bg-white rounded-xl border-[1px] border-neutral-200 overflow-hidden">
+            <div className="flex flex-row items-center gap-1 p-4">
+                <div className="text-2xl font-semibold">
+                    $ {price}
+                </div>
+                <div className="font-light text-neutral-600">
+                    night
+                </div>
+            </div>
+            <hr/>
+            <Calender
+            value={dateRange}
+            disabledDates={disabledDates}
+            onChange={(value)=> onChangeDate(value.selection)}
+            />
+            <hr/>
+            <div className="p-4">
+                <Button label="Reserve" disabled={disabled} onClick={onSubmit}/>
+            </div>
+            <div className="p-4 flex flex-row items-center justify-between font-semibold text-lg">
+                <div>Total</div>
+                <div>$ {totalPrice}</div>
+            </div>
         </div>
     );
 };
